9762ad8764818c37e464532ebf030ed6ae741897
Li Kai <linux.lik@gmail.com>
2016-12-04 22:49:18 +0800

    service & client connect

--- app/src/main/java/com/verifone/kail1/servicetest/MainActivity.java
+++ app/src/main/java/com/verifone/kail1/servicetest/MainActivity.java
@@ -1,6 +1,9 @@
 package com.verifone.kail1.servicetest;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +11,21 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Intent intent;
+    private VerifoneService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (VerifoneService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,21 +34,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btStartService = (Button) findViewById(R.id.start_service);
         Button btStopService = (Button) findViewById(R.id.stop_service);
-
         btStartService.setOnClickListener(this);
         btStopService.setOnClickListener(this);
 
-        intent = new Intent(this, VerifoneService.class);
+        Button btBindService = (Button) findViewById(R.id.bind_service);
+        Button btUnbindService = (Button) findViewById(R.id.unbind_service);
+        btBindService.setOnClickListener(this);
+        btUnbindService.setOnClickListener(this);
     }
 
     @Override
     public void onClick (View v) {
         switch (v.getId()) {
             case R.id.start_service:
-                startService(intent);
+                Intent startIntent = new Intent(this, VerifoneService.class);
+                startService(startIntent);
                 break;
             case R.id.stop_service:
-                stopService(intent);
+                Intent stopIntent = new Intent(this, VerifoneService.class);
+                stopService(stopIntent);
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, VerifoneService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE);
+                break;
+            case R.id.unbind_service:
+                unbindService(connection);
                 break;
             default:
                 break;
--- app/src/main/java/com/verifone/kail1/servicetest/VerifoneService.java
+++ app/src/main/java/com/verifone/kail1/servicetest/VerifoneService.java
@@ -2,30 +2,48 @@ package com.verifone.kail1.servicetest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 /**
  * Created by KaiL1 on 2016/12/4.
  */
 
 public class VerifoneService extends Service {
+
+    private DownloadBinder verifoneBinder = new DownloadBinder();
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d("--->VerifoneService", "startDownload executed");
+        }
+
+        public int getProgress() {
+            Log.d("--->VerifoneService", "getProgress executed");
+            return 0;
+        }
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return verifoneBinder;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("--->VerifoneService", "onCreate executed");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("--->VerifoneService", "onStartCommand executed");
        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d("--->VerifoneService", "onDestroy executed");
     }
 }
